9151f1e198fc3224ffce418d802d38feb3c68d7b
mengxiang <mengxiang17@163.com>
2016-08-25 11:24:11 +0800

    moretest

--- test/FilenameFormat.py
+++ test/FilenameFormat.py
@@ -15,4 +15,4 @@ for b in datename:
 	t = datetime.datetime(int(datenum[0]),int(datenum[1]),int(datenum[2]))
 	new1 = 'output_'+str(t.year)+'-'+str(t.month)+'-'+str(t.day)+'-'+str(t.weekday())+'.txt'
 	if name.count(new1) == False:
-		shutil.copy(b, new1)
+		shutil.move(b, new1)
